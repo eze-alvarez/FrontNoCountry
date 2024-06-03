3484224d6d6bd034620565bb11fc65b8b29a6dc0
eze-alvarez <ezenigm@hotmail.com>
2024-06-03 17:07:07 -0300

    feat:navbar, footer

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,10 +1,10 @@
 import SectionCampaigns from "../../components/Sect-Campaigns/sectionCampaigns";
-import CardCampaign from "../../components/UI/Card-Campaign/cardCampaign";
+// import CardCampaign from "../../components/UI/Card-Campaign/cardCampaign";
 
 export default function Home() {
   return (
     <div className="w-full h-screen px-4">
-      Home
+    
       <SectionCampaigns />
     </div>
   )
