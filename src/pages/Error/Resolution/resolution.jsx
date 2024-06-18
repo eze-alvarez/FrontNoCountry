591031ecbd4e102cb4de41cecf1398bf3d2d19bf
eze-alvarez <ezenigm@hotmail.com>
2024-06-18 16:18:58 -0300

    fix: form login, register

--- src/pages/Error/Resolution/resolution.jsx
+++ src/pages/Error/Resolution/resolution.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const Resolution = () => {
     return (
-        <div className="w-full h-screen px-4 flex flex-col text-center justify-center items-center min-[376px]:hidden">
+        <div className="w-full h-screen px-4 flex flex-col text-center justify-center items-center min-[320px]:hidden">
             <h1>Error</h1>
             <p>Resoluiton Not supported</p>
             <p>:(</p>
